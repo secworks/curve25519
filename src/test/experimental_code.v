e10b5e0a155b65ab1327d4c5dacc990aa61e216d
Joachim Strömbergson <joachim@secworks.se>
2015-02-27 15:26:02 +0100

    Adding initial version of test code file.

---
+++ src/test/experimental_code.v
@@ -0,0 +1,25 @@
+//======================================================================
+//
+// experimental_code.v
+// -------------------
+// Module for testing curve25519 sub functions.
+//
+//
+// Author: Joachim Strömbergson
+// (c) 2015 Assured AB
+//
+//======================================================================
+
+
+module experimental_code ();
+
+  always @ init
+    begin
+
+    end
+
+endmodule // experimental_code
+
+//======================================================================
+// EOF experimental_code.v
+//======================================================================
